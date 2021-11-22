09a93398894ea275841243d652a6742a1c561a4e
prabhuraj1234 <68596613+prabhuraj1234@users.noreply.github.com>
2021-11-22 15:18:44 +0530

    Add files via upload

---
+++ addition/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, ans;
+            Console.WriteLine("enter 2 numbers");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            ans = a + b;
+            Console.WriteLine("addition is" + ans);
+            Console.ReadKey();
+        }
+    }
+}
