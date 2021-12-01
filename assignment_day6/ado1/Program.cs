d4a501a1157dd63c44f92846662bd39750230097
prabhuraj1234 <68596613+prabhuraj1234@users.noreply.github.com>
2021-12-01 19:51:46 +0530

    Add files via upload

---
+++ assignment_day6/ado1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ado1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = null;
+
+            try
+            {
+                con = new SqlConnection ();
+                con.ConnectionString = "Server=.;database=practice;trusted_connection=true";
+                con.Open();
+                Console.WriteLine("connection_opened");
+
+                SqlCommand cmd = new SqlCommand("select * from employee",con);
+
+
+                cmd.CommandType = CommandType.Text;
+
+              
+
+                SqlDataReader reader = cmd.ExecuteReader();
+               
+                while (reader.Read())
+                {
+                 Console.WriteLine(reader[0] + "\t" + reader[1] + "\t\t" + reader[2] + "\t\t" + reader[3] + "\t" + reader[4] );
+                }
+
+
+                Console.ReadKey();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+        
+    }
+}
